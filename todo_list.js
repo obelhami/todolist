96f7aff06408ddf162e59d49dccacfe0abe5aca0
obelhami <obelhami@e2r3p4.1337.ma>
2025-04-08 17:27:12 +0100

    hbibti fadila

---
+++ todo_list.js
@@ -0,0 +1,50 @@
+let Todo_array = {
+    name:[],
+    date:[]
+};
+const Todo_input = document.querySelector('.input_todo');
+const Date_input = document.querySelector('.input_date');
+
+function add_todo()
+{
+    if (Todo_input.value.trim() && Date_input.value.trim())
+    {
+        Todo_array.name.push(Todo_input.value);
+        Todo_array.date.push(Date_input.value);
+    }
+        // console.log('todo ' + Todo_input.value);
+        // console.log('date ' + Date_input.value);
+}
+
+function render_todo()
+{
+    let htmltodo = '';
+    for (let i = 0; i < Todo_array.name.length; i++)
+    {
+        let todo = Todo_array.name[i];
+        console.log(todo);
+        let date = Todo_array.date[i];
+        console.log(date);
+        if (todo.trim() && date.trim())
+        {
+            const html= `<p>${todo} ${date} 
+            <button onclick="
+                delete_todo(${i});
+            ">Delete</button>
+            </p>`;
+            htmltodo += html;
+            flag = 1;
+        }
+    }
+    console.log(htmltodo);
+    document.querySelector('.add_paragraph').innerHTML = htmltodo;
+        Todo_input.value = '';
+        Date_input.value = '';
+}
+
+function delete_todo(index)
+{
+    Todo_array.name.splice(index, 1);
+    render_todo();
+
+}
